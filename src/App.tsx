813d9e40403e3f561df2276eeb63da680df9db8d
xuhuang37 <xuhuang37@outlook.com>
2018-11-01 16:51:47 +0800

    eject(all):eject context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
-import * as React from 'react';
-import './App.css';
+import { Table } from "antd";
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
+
+const columns = [
+  {
+    dataIndex: "name",
+    key: "name",
+    title: "昵称"
+  },
+  {
+    dataIndex: "age",
+    key: "age",
+    title: "Age"
+  },
+  {
+    dataIndex: "address",
+    key: "address",
+    title: "Address"
+  },
+  {
+    dataIndex: "action",
+    key: "action",
+    title: "Action"
+  }
+];
+const datas = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park"
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park"
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park"
+  }
+];
 
 class App extends React.Component {
   public render() {
@@ -11,9 +55,9 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div>
+          <Table columns={columns} dataSource={datas} />
+        </div>
       </div>
     );
   }
